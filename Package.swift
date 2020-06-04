a9260da30653434178323d8438da32e153b033e3
Armin Shalchian <armin536@gmail.com>
2020-06-04 23:11:47 +0430

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -20,7 +20,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SPLark",
-            dependencies: []),
+            dependencies: [],
+            path: "Source/SPLarkController"),
         .testTarget(
             name: "SPLarkTests",
             dependencies: ["SPLark"]),
