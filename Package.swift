26a373910abcd5dce4750127cbb9cb556962ee36
Ivan Vorobei <hello@ivanvorobei.io>
2020-08-29 00:00:36 +0300

    Fix Package Description

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "SPLark",
+    name: "SPLarkController",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "SPLark",
-            targets: ["SPLark"]),
+            name: "SPLarkController",
+            targets: ["SPLarkController"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,7 +19,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "SPLark",
+            name: "SPLarkController",
             dependencies: [],
             path: "Source/SPLarkController"),
         .testTarget(
